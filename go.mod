f94770edd5825496234bc67591408e26f8898d8f
wongoo <gelnyang@163.com>
2019-12-04 17:59:48 +0800

    init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/wongoo/staticsvr
+
+go 1.13
+
+require (
+	github.com/fasthttp/router v0.5.2
+	github.com/klauspost/compress v1.9.4 // indirect
+	github.com/klauspost/cpuid v1.2.2 // indirect
+	github.com/valyala/fasthttp v1.6.0
+	github.com/vogo/logger v1.2.0
+)
---
+++ staticsvr.go
@@ -0,0 +1,54 @@
+// author: wongoo
+// since: 2019-10-08
+package main
+
+import (
+	"fmt"
+	"github.com/fasthttp/router"
+	"github.com/valyala/fasthttp"
+	"github.com/vogo/logger"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	port := 9999
+	staticDir, err := os.Getwd()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	if len(os.Args) > 1 {
+		port, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}
+	if len(os.Args) > 2 {
+		staticDir = os.Args[2]
+	}
+
+	logger.Infof("serve static on port %d at directory %s", port, staticDir)
+
+	httpRouter := router.New()
+	fs := &fasthttp.FS{
+		Root:               staticDir,
+		IndexNames:         []string{"index.html"},
+		GenerateIndexPages: false,
+		Compress:           false,
+		AcceptByteRange:    false,
+		CacheDuration:      time.Minute,
+	}
+
+	httpRouter.GET("/*filepath", fs.NewRequestHandler())
+
+	server := &fasthttp.Server{
+		Handler:            httpRouter.Handler,
+		ReadTimeout:        3600 * time.Second,
+		WriteTimeout:       3600 * time.Second,
+		MaxRequestBodySize: 1 << 20,
+	}
+
+	logger.Info(server.ListenAndServe(fmt.Sprintf(":%d", port)))
+}
